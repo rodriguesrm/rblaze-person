78bf9a34b3ca0b5f0d0cd534ce46369bd0fe98e9
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2024-06-29 21:00:30 -0300

    feat:Initial project created

---
+++ docs/release-notes.md
@@ -0,0 +1,6 @@
+# RBLaze Persons
+
+Aplicação de gestão de cadastro de pessoas
+
+### Release Notes version 0.1.0
+	- Initial project structure creation
---
+++ src/RBlaze.Person.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CleanArchTemplate.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
